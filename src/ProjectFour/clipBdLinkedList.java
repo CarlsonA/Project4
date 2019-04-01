bf96390aa3d147987f3978f43e7c5828fcf189ec
Andrew Carlson <andrewcarlson@Andrews-MacBook-Pro.local>
2019-04-01 18:26:22 -0400

    Pushing given files Took files from blackboard, put them in intellij and pushed them

---
+++ src/ProjectFour/clipBdLinkedList.java
@@ -0,0 +1,13 @@
+package ProjectFour;
+
+public class clipBdLinkedList {
+    private NodeCB top;
+    private NodeCB tail;
+
+    public clipBdLinkedList() {
+        tail = top = null;
+    }
+
+    // create methods you need.
+
+}
